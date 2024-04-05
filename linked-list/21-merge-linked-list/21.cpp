7e9aa86d10c69acb049c683be55fc28d05762c9b
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2024-04-05 10:24:50 -0300

    reorganize project by data structure

---
+++ linked-list/21-merge-linked-list/21.cpp
@@ -0,0 +1,46 @@
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+    if (!list1)
+      return list2;
+    if (!list2)
+      return list1;
+
+    ListNode *tail;
+    if (list1->val > list2->val) {
+      tail = list2;
+      list2 = list2->next;
+    } else {
+      tail = list1;
+      list1 = list1->next;
+    }
+
+    ListNode *curr = tail;
+    while (list1 && list2) {
+      if (list1->val < list2->val) {
+        curr->next = list1;
+        list1 = list1->next;
+      } else {
+        curr->next = list2;
+        list2 = list2->next;
+      }
+      curr = curr->next;
+    }
+
+    if (!list1) {
+      curr->next = list2;
+    } else {
+      curr->next = list1;
+    }
+
+    return tail;
+  }
+};
