153071544a1653b78dc026694f10a986f5feda1d
Rodrigo F. Schaer <rodrigo.fscs@gmail.com>
2024-05-13 22:33:43 -0300

    230 find kth smallest value in bst

---
+++ binary-tree/230-kth-smallest-in-bst/230.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+	val: number
+	left: TreeNode | null
+	right: TreeNode | null
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = (val === undefined ? 0 : val)
+		this.left = (left === undefined ? null : left)
+		this.right = (right === undefined ? null : right)
+	}
+}
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+	let orderedArr: number[] = [];
+	function inorderT(root: TreeNode | null): void {
+		if (!root) return;
+
+		inorderT(root.left);
+		orderedArr.push(root.val);
+		inorderT(root.right);
+	}
+
+	inorderT(root);
+	return orderedArr[k - 1];
+};
